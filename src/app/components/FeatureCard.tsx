07b9e117a649649f999f98de0353090414ce3f3f
nathan <nathanadrian2002@gmail.com>
2025-11-16 20:30:41 +0530

    more more more

--- src/app/components/FeatureCard.tsx
+++ src/app/components/FeatureCard.tsx
@@ -10,7 +10,7 @@ export default function FeatureCard({
   return (
     <div
       className="
-        px-4 pt-4 pb-[25px]
+        px-6 pt-5 pb-[25px]
       bg-pink-100/30 rounded-[10px]
         border-2 sm:border-3 border-pink-100/60
       "
@@ -18,10 +18,12 @@ export default function FeatureCard({
       {/* Card */}
       <div className="flex flex-row items-center gap-[10px] mb-[5px] md:items-start md:flex-col">
         {icon}
-        <h5 className="text-4 text-gray-950 font-bold">{title}</h5>
+        <h5 className="text-base sm:text-xl text-gray-950 font-bold">
+          {title}
+        </h5>
       </div>
 
-      <p className="mr-[10px] text-[14px] text-pink-950 leading-normal font-regular">
+      <p className="mr-[10px] text-base sm:text-xl text-pink-950 leading-normal font-regular">
         {description}
       </p>
     </div>
